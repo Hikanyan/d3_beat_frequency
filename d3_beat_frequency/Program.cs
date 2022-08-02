c3f58e771fb9db7557b706d9d6da4b41274044d2
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 20:46:37 +0900

    プロジェクト ファイルを追加します。

---
+++ d3_beat_frequency/Program.cs
@@ -0,0 +1,14 @@
+﻿//https://paiza.jp/works/mondai/drankfast/d3_beat_frequency
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
